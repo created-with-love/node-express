025217ca7b6529621abded1a50929bb73c4664e0
Volodymyr Ivanchenko <vladymyrivanchenko3@gmail.com>
2023-10-30 23:27:20 +0200

    chore: delete adv from readme

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,1 +1,10 @@
-console.log('Express Tutorial')
+const http = require('http')
+
+// 4:16:20
+const server = http.createServer((req,res) => {
+    console.log('user hit the server')
+
+    res.end('Wake up, Neo...')
+})
+
+server.listen(5000)
